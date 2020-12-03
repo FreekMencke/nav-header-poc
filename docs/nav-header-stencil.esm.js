35d1bd646649e90c9c41ba9f3e13e85edd7a40a8
Freek Mencke <freek_mencke@outlook.com>
2020-12-03 11:04:07 +0100

    remove shadow

--- docs/nav-header-stencil.esm.js
+++ docs/nav-header-stencil.esm.js
@@ -1,1 +1,1 @@
-import{p as o,b as p}from"./p-30a85843.js";(()=>{const p=import.meta.url,t={};return""!==p&&(t.resourcesUrl=new URL(".",p).href),o(t)})().then((o=>p([["p-783f33fa",[[1,"my-component",{apps:[32],alerts:[32],dropdownVisible:[32],addAlert:[64]},[[8,"pushstate","onRouteChange"]]]]]],o)));
+import{p as e,b as o}from"./p-30a85843.js";(()=>{const o=import.meta.url,p={};return""!==o&&(p.resourcesUrl=new URL(".",o).href),e(p)})().then((e=>o([["p-7e368abb",[[1,"my-component",{apps:[32],alerts:[32],dropdownVisible:[32],addAlert:[64]},[[8,"pushstate","onRouteChange"]]]]]],e)));
